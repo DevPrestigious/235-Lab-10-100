31185dcbcc721f1dcf773ba5c3fc62557ce58e96
Costigan-Stephen <stephenjcostigan@hotmail.com>
2021-11-15 20:27:53 -0600

    added name

--- priority_queue.h
+++ priority_queue.h
@@ -13,7 +13,7 @@
  *    This will contain the class definition of:
  *        priority_queue          : A class that represents a Priority Queue
  * Author
- *    <your names here>
+ *    Stephen Costigan
  ************************************************************************/
 
 #pragma once
@@ -24,127 +24,127 @@
 namespace custom
 {
 
-/*************************************************
- * P QUEUE
- * Create a priority queue.
- *************************************************/
-template<class T>
-class priority_queue
-{
-public:
+    /*************************************************
+     * P QUEUE
+     * Create a priority queue.
+     *************************************************/
+    template<class T>
+    class priority_queue
+    {
+    public:
 
-   //
-   // construct
-   //
-   priority_queue() 
-   {
-   }
-   priority_queue(const priority_queue &  rhs)  
-   { 
-   }
-   priority_queue(priority_queue && rhs)  
-   { 
-   }
-   template <class Iterator>
-   priority_queue(Iterator first, Iterator last) 
-   {
-   }
-   explicit priority_queue (custom::vector<T> && rhs) 
-   {
-   }
-   explicit priority_queue (custom::vector<T>& rhs)
-   {
-   }
-  ~priority_queue() {}
+        //
+        // construct
+        //
+        priority_queue()
+        {
+        }
+        priority_queue(const priority_queue& rhs)
+        {
+        }
+        priority_queue(priority_queue&& rhs)
+        {
+        }
+        template <class Iterator>
+        priority_queue(Iterator first, Iterator last)
+        {
+        }
+        explicit priority_queue(custom::vector<T>&& rhs)
+        {
+        }
+        explicit priority_queue(custom::vector<T>& rhs)
+        {
+        }
+        ~priority_queue() {}
 
-   //
-   // Access
-   //
-   const T & top() const;
+        //
+        // Access
+        //
+        const T& top() const;
 
-   //
-   // Insert
-   //
-   void  push(const T& t);
-   void  push(T&& t);     
+        //
+        // Insert
+        //
+        void  push(const T& t);
+        void  push(T&& t);
 
-   //
-   // Remove
-   //
-   void  pop(); 
+        //
+        // Remove
+        //
+        void  pop();
 
-   //
-   // Status
-   //
-   size_t size()  const 
-   { 
-      return 99;   
-   }
-   bool empty() const 
-   { 
-      return false;  
-   }
-   
+        //
+        // Status
+        //
+        size_t size()  const
+        {
+            return 99;
+        }
+        bool empty() const
+        {
+            return false;
+        }
+
 #ifdef DEBUG // make this visible to the unit tests
-public:
+    public:
 #else
-private:
+    private:
 #endif
 
-   bool percolateDown(size_t indexHeap);      // fix heap from index down. This is a heap index!
+        bool percolateDown(size_t indexHeap);      // fix heap from index down. This is a heap index!
 
-   custom::vector<T> container; 
+        custom::vector<T> container;
+
+    };
+
+    /************************************************
+     * P QUEUE :: TOP
+     * Get the maximum item from the heap: the top item.
+     ***********************************************/
+    template <class T>
+    const T& priority_queue <T> ::top() const
+    {
+        return *(new T);
+    }
+
+    /**********************************************
+     * P QUEUE :: POP
+     * Delete the top item from the heap.
+     **********************************************/
+    template <class T>
+    void priority_queue <T> ::pop()
+    {
+    }
+
+    /*****************************************
+     * P QUEUE :: PUSH
+     * Add a new element to the heap, reallocating as necessary
+     ****************************************/
+    template <class T>
+    void priority_queue <T> ::push(const T& t)
+    {
+    }
+    template <class T>
+    void priority_queue <T> ::push(T&& t)
+    {
+    }
+
+    /************************************************
+     * P QUEUE :: PERCOLATE DOWN
+     * The item at the passed index may be out of heap
+     * order. Take care of that little detail!
+     * Return TRUE if anything changed.
+     ************************************************/
+    template <class T>
+    bool priority_queue <T> ::percolateDown(size_t indexHeap)
+    {
+        return false;
+    }
 
 };
 
-/************************************************
- * P QUEUE :: TOP
- * Get the maximum item from the heap: the top item.
- ***********************************************/
 template <class T>
-const T & priority_queue <T> :: top() const
-{
-   return *(new T);
-}
-
-/**********************************************
- * P QUEUE :: POP
- * Delete the top item from the heap.
- **********************************************/
-template <class T>
-void priority_queue <T> :: pop()
-{
-}
-
-/*****************************************
- * P QUEUE :: PUSH
- * Add a new element to the heap, reallocating as necessary
- ****************************************/
-template <class T>
-void priority_queue <T> :: push(const T & t)
-{
-}
-template <class T>
-void priority_queue <T> :: push(T && t)
-{
-}
-
-/************************************************
- * P QUEUE :: PERCOLATE DOWN
- * The item at the passed index may be out of heap
- * order. Take care of that little detail!
- * Return TRUE if anything changed.
- ************************************************/
-template <class T>
-bool priority_queue <T> :: percolateDown(size_t indexHeap)
-{
-   return false;
-}
-
-};
-
-template <class T>
-inline void swap(custom::priority_queue <T> & lhs,
-                 custom::priority_queue <T> & rhs)
+inline void swap(custom::priority_queue <T>& lhs,
+    custom::priority_queue <T>& rhs)
 {
 }
